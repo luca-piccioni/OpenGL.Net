c2ca8472c7d5f93bac9d2b7564301da5764371de
Luca Piccioni <luca.piccioni@gmail.com>
2016-02-09 21:24:17 +0100

    Added scripts and binaries to download and generate terrain data required for running example.

---
+++ Samples/HelloWorld/Data/DownloadTerrain.bat
@@ -0,0 +1,14 @@
+
+SET BASE_WWW=http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia
+
+.\wget.exe %BASE_WWW%/N44E009.hgt.zip
+.\wget.exe %BASE_WWW%/N44E010.hgt.zip
+.\wget.exe %BASE_WWW%/N44E011.hgt.zip
+.\wget.exe %BASE_WWW%/N45E009.hgt.zip
+.\wget.exe %BASE_WWW%/N45E010.hgt.zip
+.\wget.exe %BASE_WWW%/N45E011.hgt.zip
+.\wget.exe %BASE_WWW%/N46E009.hgt.zip
+.\wget.exe %BASE_WWW%/N44E010.hgt.zip
+.\wget.exe %BASE_WWW%/N44E011.hgt.zip
+
+pause
---
+++ Samples/HelloWorld/Data/UpdareVT.bat
@@ -0,0 +1,6 @@
+dir /b *.hgt > TerrainFileList.txt
+
+.\gdalbuildvrt.exe Terrain.vrt -input_file_list TerrainFileList.txt
+.\gdaladdo.exe -r average Terrain.vrt 2 4 8 16 32 64 128 256 512 1024 2048
+
+pause
