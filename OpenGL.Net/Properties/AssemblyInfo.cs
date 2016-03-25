58f95ce61ea1a5312e9c7c8c93cf04ecbe928958
Luca Piccioni <luca.piccioni@gmail.com>
2016-03-25 20:33:02 +0100

    Updated versions (0.2.0).

--- OpenGL.Net/Properties/AssemblyInfo.cs
+++ OpenGL.Net/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.2.0")]
