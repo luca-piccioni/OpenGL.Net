247f02c630ade119b4cc7393f3a1ceedf9188b14
Luca Piccioni <luca.piccioni@gmail.com>
2016-04-03 22:37:03 +0200

    Hello triangle.

---
+++ Samples/HelloTriangle/SampleForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace HelloTriangle
+{
+	partial class SampleForm
+	{
+		/// <summary>
+		/// Variabile di progettazione necessaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Pulire le risorse in uso.
+		/// </summary>
+		/// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Codice generato da Progettazione Windows Form
+
+		/// <summary>
+		/// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+		/// il contenuto del metodo con l'editor di codice.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Graphics = new OpenGL.GraphicsControl();
+			this.SuspendLayout();
+			// 
+			// Graphics
+			// 
+			this.Graphics.ContextFlags = OpenGL.GraphicsContextFlags.CompatibilityProfile;
+			this.Graphics.DepthBits = ((uint)(0u));
+			this.Graphics.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Graphics.Location = new System.Drawing.Point(0, 0);
+			this.Graphics.MultisampleBits = ((uint)(0u));
+			this.Graphics.Name = "Graphics";
+			this.Graphics.Size = new System.Drawing.Size(604, 428);
+			this.Graphics.StencilBits = ((uint)(0u));
+			this.Graphics.TabIndex = 0;
+			this.Graphics.GraphicsContextCreated += new System.EventHandler<OpenGL.GraphicsControlEventArgs>(this.Graphics_GraphicsContextCreated);
+			this.Graphics.Render += new System.EventHandler<OpenGL.GraphicsControlEventArgs>(this.Graphics_Render);
+			// 
+			// SampleForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(604, 428);
+			this.Controls.Add(this.Graphics);
+			this.Name = "SampleForm";
+			this.Text = "Hello triangle";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private OpenGL.GraphicsControl Graphics;
+	}
+}
+
---
+++ Samples/HelloTriangle/SampleForm.cs
@@ -0,0 +1,63 @@
+﻿
+// Copyright (C) 2016 Luca Piccioni
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+// USA
+
+using System;
+using System.Windows.Forms;
+
+using OpenGL;
+
+namespace HelloTriangle
+{
+	/// <summary>
+	/// Hello triangle sample.
+	/// </summary>
+	public partial class SampleForm : Form
+	{
+		/// <summary>
+		/// Construct a SampleForm.
+		/// </summary>
+		public SampleForm()
+		{
+			InitializeComponent();
+		}
+
+		private void Graphics_GraphicsContextCreated(object sender, GraphicsControlEventArgs e)
+		{
+			e.Framebuffer.SetClearColor(new ColorRGBAF(0.2f, 0.2f, 0.2f));
+		}
+
+		private void Graphics_Render(object sender, GraphicsControlEventArgs e)
+		{
+			// Old school OpenGL
+			Gl.MatrixMode(MatrixMode.Projection);
+			Gl.LoadIdentity();
+			Gl.Ortho(0.0, 1.0f, 0.0, 1.0, 0.0, 1.0);
+
+			Gl.MatrixMode(MatrixMode.Modelview);
+			Gl.LoadIdentity();
+
+			Gl.Clear(ClearBufferMask.ColorBufferBit);
+
+			Gl.Begin(PrimitiveType.Triangles);
+			Gl.Color3(1.0f, 0.0f, 0.0f); Gl.Vertex2(0.0f, 0.0f);
+			Gl.Color3(0.0f, 1.0f, 0.0f); Gl.Vertex2(0.5f, 1.0f);
+			Gl.Color3(0.0f, 0.0f, 1.0f); Gl.Vertex2(1.0f, 0.0f);
+			Gl.End();
+		}
+	}
+}
