2d377cb2b698a053c3125b0adbf38c4f2280d193
Luca Piccioni <luca.piccioni@gmail.com>
2016-04-10 15:50:21 +0200

    Implemented abstraction for *ChoosePixelFormat on DevicePixelFormatCollection.

--- OpenGL.Net/DevicePixelFormatCollection.cs
+++ OpenGL.Net/DevicePixelFormatCollection.cs
@@ -28,6 +28,96 @@ namespace OpenGL
 	[DebuggerDisplay("DevicePixelFormatCollection: Count={Count}")]
 	public class DevicePixelFormatCollection : List<DevicePixelFormat>
 	{
-		
+		/// <summary>
+		/// Choose a <see cref="DevicePixelFormat"/>
+		/// </summary>
+		/// <param name="pixelFormat">
+		/// A <see cref="DevicePixelFormat"/> that specify the minimum requirements
+		/// </param>
+		/// <returns></returns>
+		public List<DevicePixelFormat> Choose(DevicePixelFormat pixelFormat)
+		{
+			if (pixelFormat == null)
+				throw new ArgumentNullException("pixelFormat");
+
+			List<DevicePixelFormat> pixelFormats = new List<DevicePixelFormat>(this);
+
+			pixelFormats.RemoveAll(delegate (DevicePixelFormat item) {
+				if (pixelFormat.RgbaUnsigned && !item.RgbaUnsigned)
+					return (true);
+				if (pixelFormat.RgbaFloat && !item.RgbaFloat)
+					return (true);
+
+				if (pixelFormat.RenderWindow && !item.RenderWindow)
+					return (true);
+				if (pixelFormat.RenderPBuffer && !item.RenderPBuffer)
+					return (true);
+				if (pixelFormat.RenderBuffer && !item.RenderBuffer)
+					return (true);
+
+				if (pixelFormat.SRGBCapable && !item.SRGBCapable)
+					return (true);
+				if (pixelFormat.DoubleBuffer && !item.DoubleBuffer)
+					return (true);
+
+				if (item.ColorBits < pixelFormat.ColorBits)
+					return (true);
+				if (item.RedBits < pixelFormat.RedBits)
+					return (true);
+				if (item.GreenBits < pixelFormat.GreenBits)
+					return (true);
+				if (item.BlueBits < pixelFormat.BlueBits)
+					return (true);
+				if (item.AlphaBits < pixelFormat.AlphaBits)
+					return (true);
+
+				if (item.DepthBits < pixelFormat.DepthBits)
+					return (true);
+				if (item.StencilBits < pixelFormat.StencilBits)
+					return (true);
+
+				return (false);
+			});
+
+			List<DevicePixelFormat> pixelFormatsCopy = new List<DevicePixelFormat>();
+
+			foreach (DevicePixelFormat devicePixelFormat in pixelFormats)
+				pixelFormatsCopy.Add(devicePixelFormat.Copy());
+
+			// Sort (ascending by resource occupation)
+			pixelFormatsCopy.Sort(delegate (DevicePixelFormat x, DevicePixelFormat y) {
+				int compare;
+
+				if ((compare = x.ColorBits.CompareTo(y.ColorBits)) != 0)
+					return (compare);
+				if ((compare = x.DepthBits.CompareTo(y.DepthBits)) != 0)
+					return (compare);
+				if ((compare = x.StencilBits.CompareTo(y.StencilBits)) != 0)
+					return (compare);
+
+				if ((compare = y.DoubleBuffer.CompareTo(x.DoubleBuffer)) != 0)
+					return (compare);
+
+				return (0);
+			});
+
+			return (pixelFormatsCopy);
+		}
+
+		/// <summary>
+		/// Copy this DevicePixelFormatCollection.
+		/// </summary>
+		/// <returns>
+		/// It returns a <see cref="DevicePixelFormatCollection"/> equivalent to this DevicePixelFormatCollection.
+		/// </returns>
+		public DevicePixelFormatCollection Copy()
+		{
+			DevicePixelFormatCollection pixelFormats = new DevicePixelFormatCollection();
+
+			foreach (DevicePixelFormat devicePixelFormat in this)
+				pixelFormats.Add(devicePixelFormat.Copy());
+
+			return (pixelFormats);
+		}
 	}
 }
