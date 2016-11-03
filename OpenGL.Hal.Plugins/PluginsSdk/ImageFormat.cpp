0d77eb2abf9d1ddf0cf02f507e793f09937f247a
Luca <luca.piccioni@gmail.com>
2016-11-03 21:00:41 +0100

    Clean master: removed many funny projects.

--- OpenGL.Hal.Plugins/PluginsSdk/ImageFormat.cpp
+++
@@ -1,4 +0,0 @@
-
-#include "ImageFormat.h"
-
-const char *ImageFormat_Bitmap = "Bitmap";
--- Samples/HelloWorld/Data/UpdareVT.bat
+++
@@ -1,6 +0,0 @@
-dir /b *.hgt > TerrainFileList.txt
-
-.\gdalbuildvrt.exe Terrain.vrt -input_file_list TerrainFileList.txt
-.\gdaladdo.exe -r average Terrain.vrt 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384
-
-pause
