9be0fa4d931241d9bda56edc43bd89df5ea86eb8
Luca Piccioni <luca.piccioni@gmail.com>
2017-04-18 23:30:32 +0200

    Update nuget-push.bat

--- nuget-push.bat
+++ nuget-push.bat
@@ -1,3 +1,3 @@
-nuget.exe push "%1" -ApiKey ad192d16-7941-4f96-a67f-261bf2e4f524 -Source https://www.nuget.org/api/v2/package
+nuget.exe push "%1" -ApiKey xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx -Source https://www.nuget.org/api/v2/package
 
 pause
