2faac2aa60543c0484867c64e7cb0dad35449b8f
Luca Piccioni <luca.piccioni@gmail.com>
2017-05-01 11:06:33 +0200

    Preparing nuget packages 0.5.0.

--- nuget-pack.bat
+++ nuget-pack.bat
@@ -1,4 +1,5 @@
 nuget.exe pack OpenGL.Net.nuspec -Symbol
+nuget.exe pack OpenGL.Net.Debug.nuspec -Symbol
 nuget.exe pack OpenGL.Net.WinForms.nuspec -Symbol
 nuget.exe pack OpenGL.Net.Xamarin.Android.nuspec -Symbol
 nuget.exe pack OpenGL.Net.VideoCore.nuspec -Symbol
