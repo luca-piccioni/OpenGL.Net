5b50d945ccb880e56aa3768927631644d864df04
Luca Piccioni <luca.piccioni@gmail.com>
2017-05-17 01:55:22 +0200

    Updated .nuspec files for next nuget package.

--- nuget-pack.bat
+++
@@ -1,9 +0,0 @@
-nuget.exe pack OpenGL.Net.nuspec -Symbol
-nuget.exe pack OpenGL.Net.Debug.nuspec -Symbol
-nuget.exe pack OpenGL.Net.WinForms.nuspec -Symbol
-nuget.exe pack OpenGL.Net.Xamarin.Android.nuspec -Symbol
-nuget.exe pack OpenGL.Net.VideoCore.nuspec -Symbol
-nuget.exe pack OpenGL.Net.CoreProfile.nuspec -Symbol
-nuget.exe pack OpenGL.Net.ES2Profile.nuspec -Symbol
-
-pause
