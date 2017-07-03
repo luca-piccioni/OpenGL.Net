7ab7e2338c509441f4653695dcfdd8dca3a1a05f
Luca Piccioni <luca.piccioni@gmail.com>
2017-07-03 21:12:45 +0200

    First draft of the complete GTK3 widget.

--- OpenGL.Net.GTK3/GlWidget.cs
+++ OpenGL.Net.GTK3/GlWidget.cs
@@ -20,11 +20,14 @@
 #pragma warning disable 612
 
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Security;
 
 using Cairo;
+using GLib;
 using Gtk;
 
 namespace OpenGL.GTK3
@@ -47,7 +50,86 @@ namespace OpenGL.GTK3
 
 		#endregion
 
-		#region Designer Properties
+		#region Design Properties - Context Attributes
+
+		#endregion
+
+		#region Design Properties - Framebuffer
+
+		/// <summary>
+		/// Get or set the OpenGL minimum color buffer bits.
+		/// </summary>
+		[Property("color-bits")]
+		public uint ColorBits
+		{
+			get { return (_ColorBits); }
+			set { _ColorBits = value; }
+		}
+
+		/// <summary>
+		/// The OpenGL color buffer bits.
+		/// </summary>
+		private uint _ColorBits = 24;
+
+		/// <summary>
+		/// Get or set the OpenGL minimum depth buffer bits.
+		/// </summary>
+		[Property("depth-bits")]
+		public uint DepthBits
+		{
+			get { return (_DepthBits); }
+			set { _DepthBits = value; }
+		}
+
+		/// <summary>
+		/// The OpenGL color buffer bits.
+		/// </summary>
+		private uint _DepthBits;
+
+		/// <summary>
+		/// Get or set the OpenGL minimum stencil buffer bits.
+		/// </summary>
+		[Property("stencil-bits")]
+		public uint StencilBits
+		{
+			get { return (_StencilBits); }
+			set { _StencilBits = value; }
+		}
+
+		/// <summary>
+		/// The OpenGL color buffer bits.
+		/// </summary>
+		private uint _StencilBits;
+
+		/// <summary>
+		/// Get or set the OpenGL minimum multisample buffer "bits".
+		/// </summary>
+		[Property("multisample-bits")]
+		public uint MultisampleBits
+		{
+			get { return (_MultisampleBits); }
+			set { _MultisampleBits = value; }
+		}
+
+		/// <summary>
+		/// The OpenGL multisample buffer bits.
+		/// </summary>
+		private uint _MultisampleBits;
+
+		/// <summary>
+		/// Get or set the OpenGL swap buffers interval.
+		/// </summary>
+		[Property("swap-interval")]
+		public int SwapInterval
+		{
+			get { return (_SwapInterval); }
+			set { _SwapInterval = value; }
+		}
+
+		/// <summary>
+		/// The OpenGL swap buffers interval.
+		/// </summary>
+		private int _SwapInterval = 1;
 
 		/// <summary>
 		/// The <see cref="DevicePixelFormat"/> describing the minimum pixel format required by this control.
@@ -61,10 +143,10 @@ namespace OpenGL.GTK3
 				controlReqFormat.RgbaUnsigned = true;
 				controlReqFormat.RenderWindow = true;
 
-				controlReqFormat.ColorBits = (int)24;
-				controlReqFormat.DepthBits = (int)0;
-				controlReqFormat.StencilBits = (int)0;
-				controlReqFormat.MultisampleBits = (int)0;
+				controlReqFormat.ColorBits = (int)ColorBits;
+				controlReqFormat.DepthBits = (int)DepthBits;
+				controlReqFormat.StencilBits = (int)StencilBits;
+				controlReqFormat.MultisampleBits = (int)MultisampleBits;
 				controlReqFormat.DoubleBuffer = true;
 
 				return (controlReqFormat);
@@ -73,42 +155,90 @@ namespace OpenGL.GTK3
 
 		#endregion
 
-		#region DrawingArea Overrides
+		#region Device Context
 
-		protected override void OnRealized()
+		/// <summary>
+		/// Get the <see cref="DeviceContext"/> created on this GlWidget.
+		/// </summary>
+		public DeviceContext Device
 		{
-			// Base implementation
-			base.OnRealized();
-
-			// Create Device and GL context
-			IntPtr winHandle = GetWindowHandle();
-
-			if (winHandle != IntPtr.Zero) {
-				_DeviceContext = DeviceContext.Create(IntPtr.Zero, GetWindowHandle());
-
-				DevicePixelFormat[] pixelFormats = _DeviceContext.PixelsFormats.Choose(ControlPixelFormat).ToArray();
-
-				_DeviceContext.SetPixelFormat(pixelFormats[0]);
-
-				_GraphicsContext = _DeviceContext.CreateContext(IntPtr.Zero);
-			}
+			get { return (_DeviceContext); }
 		}
 
-		protected override bool OnDrawn(Context cr)
+		/// <summary>
+		/// Create the device context and set the pixel format.
+		/// </summary>
+		protected void CreateDeviceContext()
 		{
-			// Base implementation
-			//bool ret = base.OnDrawn(cr);
+			CreateDeviceContext(ControlPixelFormat);
+		}
 
-			_DeviceContext.MakeCurrent(_GraphicsContext);
+		/// <summary>
+		/// Create the device context and set the pixel format.
+		/// </summary>
+		private void CreateDeviceContext(DevicePixelFormat controlReqFormat)
+		{
+			#region Create device context
 
-			Gl.ClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+			_DeviceContext = DeviceContext.Create(GetDisplay(), GetWindowHandle());
+			_DeviceContext.IncRef();
 
-			Gl.Viewport(0, 0, 100, 100);
-			Gl.Clear(ClearBufferMask.ColorBufferBit);
+			#endregion
 
-			_DeviceContext.SwapBuffers();
+			#region Set pixel format
 
-			return (true);
+			DevicePixelFormatCollection pixelFormats = _DeviceContext.PixelsFormats;
+			List<DevicePixelFormat> matchingPixelFormats = pixelFormats.Choose(controlReqFormat);
+
+			if ((matchingPixelFormats.Count == 0) && controlReqFormat.MultisampleBits > 0) {
+				// Try to select the maximum multisample configuration
+				int multisampleBits = 0;
+
+				pixelFormats.ForEach(delegate (DevicePixelFormat item) { multisampleBits = Math.Max(multisampleBits, item.MultisampleBits); });
+
+				controlReqFormat.MultisampleBits = multisampleBits;
+
+				matchingPixelFormats = pixelFormats.Choose(controlReqFormat);
+			}
+
+			if ((matchingPixelFormats.Count == 0) && controlReqFormat.DoubleBuffer) {
+				// Try single buffered pixel formats
+				controlReqFormat.DoubleBuffer = false;
+
+				matchingPixelFormats = pixelFormats.Choose(controlReqFormat);
+				if (matchingPixelFormats.Count == 0)
+					throw new InvalidOperationException(String.Format("unable to find a suitable pixel format: {0}", pixelFormats.GuessChooseError(controlReqFormat)));
+			} else if (matchingPixelFormats.Count == 0)
+				throw new InvalidOperationException(String.Format("unable to find a suitable pixel format: {0}", pixelFormats.GuessChooseError(controlReqFormat)));
+
+			_DeviceContext.SetPixelFormat(matchingPixelFormats[0]);
+
+			#endregion
+
+			#region Set V-Sync
+
+			if (Gl.PlatformExtensions.SwapControl) {
+				int swapInterval = SwapInterval;
+
+				// Mask value in case it is not supported
+				if (!Gl.PlatformExtensions.SwapControlTear && swapInterval == -1)
+					swapInterval = 1;
+
+				_DeviceContext.SwapInterval(swapInterval);
+			}
+
+			#endregion
+		}
+
+		/// <summary>
+		/// Get the display associated with this Widget.
+		/// </summary>
+		/// <returns>
+		/// It returns an <see cref="IntPtr"/> valid for GLX functions as display parameter.
+		/// </returns>
+		private IntPtr GetDisplay()
+		{
+			return (IntPtr.Zero);
 		}
 
 		/// <summary>
@@ -135,10 +265,273 @@ namespace OpenGL.GTK3
 		/// </summary>
 		private DeviceContext _DeviceContext;
 
+		#endregion
+
+		#region OpenGL Context
+
+		/// <summary>
+		/// Create the GlWidget context.
+		/// </summary>
+		private void CreateContext()
+		{
+			if (_GraphicsContext != IntPtr.Zero)
+				throw new InvalidOperationException("context already created");
+
+			IntPtr sharingContext = IntPtr.Zero;
+
+			if (Gl.PlatformExtensions.CreateContext_ARB) {
+				List<int> attributes = new List<int>();
+				uint contextProfile = 0, contextFlags = 0;
+				bool debuggerAttached = Debugger.IsAttached;
+
+				#region WGL_ARB_create_context|GLX_ARB_create_context
+
+				#endregion
+
+				#region WGL_ARB_create_context_profile|GLX_ARB_create_context_profile
+
+				if (Gl.PlatformExtensions.CreateContextProfile_ARB) {
+
+				}
+
+				#endregion
+
+				#region WGL_ARB_create_context_robustness|GLX_ARB_create_context_robustness
+
+				if (Gl.PlatformExtensions.CreateContextRobustness_ARB) {
+
+				}
+
+				#endregion
+
+				Debug.Assert(Wgl.CONTEXT_FLAGS_ARB == Glx.CONTEXT_FLAGS_ARB);
+				if (contextFlags != 0)
+					attributes.AddRange(new int[] { Wgl.CONTEXT_FLAGS_ARB, unchecked((int)contextFlags) });
+
+				Debug.Assert(Wgl.CONTEXT_PROFILE_MASK_ARB == Glx.CONTEXT_PROFILE_MASK_ARB);
+				Debug.Assert(contextProfile == 0 || Gl.PlatformExtensions.CreateContextProfile_ARB);
+				if (contextProfile != 0)
+					attributes.AddRange(new int[] { Wgl.CONTEXT_PROFILE_MASK_ARB, unchecked((int)contextProfile) });
+
+				attributes.Add(0);
+
+				if ((_GraphicsContext = _DeviceContext.CreateContextAttrib(sharingContext, attributes.ToArray())) == IntPtr.Zero)
+					throw new InvalidOperationException(String.Format("unable to create render context ({0})", Gl.GetError()));
+			} else {
+				// Create OpenGL context using compatibility profile
+				if ((_GraphicsContext = _DeviceContext.CreateContext(sharingContext)) == IntPtr.Zero)
+					throw new InvalidOperationException("unable to create render context");
+			}
+		}
+
+		/// <summary>
+		/// Make the GlWidget context current.
+		/// </summary>
+		private void MakeCurrentContext()
+		{
+			// Make context current
+			if (_DeviceContext.MakeCurrent(_GraphicsContext) == false)
+				throw new InvalidOperationException("unable to make context current");
+		}
+
+		/// <summary>
+		/// Delete the GlWidget context.
+		/// </summary>
+		private void DeleteContext()
+		{
+			// Delete OpenGL context
+			if (_GraphicsContext != IntPtr.Zero) {
+				_DeviceContext.DeleteContext(_GraphicsContext);
+				_GraphicsContext = IntPtr.Zero;
+			}
+		}
+
 		/// <summary>
 		/// OpenGL context created on <see cref="_DeviceContext"/>.
 		/// </summary>
 		private IntPtr _GraphicsContext;
+
+		#endregion
+
+		#region Events
+
+		/// <summary>
+		/// Event raised on control creation time, allow user to allocate resource on control.
+		/// </summary>
+		[Signal("context-created")]
+		public event EventHandler<GlWidgetEventArgs> ContextCreated
+		{
+			add { _ContextCreated += value; }
+			remove { _ContextCreated -= value; }
+		}
+
+		/// <summary>
+		/// Underlying EventHandler for <see cref="ContextCreated"/>.
+		/// </summary>
+		private EventHandler<GlWidgetEventArgs> _ContextCreated;
+
+		/// <summary>
+		/// Raise the event <see cref="ContextCreated"/>.
+		/// </summary>
+		protected virtual void OnContextCreated()
+		{
+			if (_ContextCreated != null) {
+				GlWidgetEventArgs glControlEventArgs = new GlWidgetEventArgs(_DeviceContext, _GraphicsContext);
+
+				foreach (EventHandler<GlWidgetEventArgs> handler in _ContextCreated.GetInvocationList()) {
+					try {
+						handler(this, glControlEventArgs);
+					} catch (Exception exception) {
+						Debug.Fail(String.Format("OnContextCreated: exception ({0})\n{1}", exception.Message, exception.ToString()));
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Event raised on control disposition time, allow user to dispose resources on control.
+		/// </summary>
+		[Signal("context-destroying")]
+		public event EventHandler<GlWidgetEventArgs> ContextDestroying;
+
+		/// <summary>
+		/// Raise the event <see cref="ContextDestroying"/>.
+		/// </summary>
+		protected virtual void OnContextDestroying()
+		{
+			if (ContextDestroying != null)
+				ContextDestroying(this, new GlWidgetEventArgs(_DeviceContext, _GraphicsContext));
+		}
+
+		/// <summary>
+		/// Event raised on control render time, allow user to draw on control.
+		/// </summary>
+		[Signal("render")]
+		public event EventHandler<GlWidgetEventArgs> Render;
+
+		/// <summary>
+		/// Raise the event <see cref="Render"/>.
+		/// </summary>
+		protected virtual void OnRender()
+		{
+			if (Render != null && _GraphicsContext != IntPtr.Zero)
+				Render(this, new GlWidgetEventArgs(_DeviceContext, _GraphicsContext));
+		}
+
+		/// <summary>
+		/// Event raised on control render time, allow user to update resources. It is executed AFTER the <see cref="Render"/>
+		/// event.
+		/// </summary>
+		[Signal("context-update")]
+		public event EventHandler<GlWidgetEventArgs> ContextUpdate;
+
+		/// <summary>
+		/// Raise the event <see cref="ContextUpdate"/>.
+		/// </summary>
+		protected virtual void OnContextUpdate()
+		{
+			if (ContextUpdate != null && _GraphicsContext != IntPtr.Zero)
+				ContextUpdate(this, new GlWidgetEventArgs(_DeviceContext, _GraphicsContext));
+		}
+
+		#endregion
+
+		#region DrawingArea Overrides
+
+		protected override void OnRealized()
+		{
+			// Base implementation
+			base.OnRealized();
+
+			// Create device context
+			CreateDeviceContext();
+			// Create OpenGL context
+			CreateContext();
+			// The context is made current unconditionally: event handlers allocate resources
+			MakeCurrentContext();
+			// Event handling
+			OnContextCreated();
+		}
+
+		protected override void OnUnrealized()
+		{
+			if (_GraphicsContext != IntPtr.Zero) {
+				// Event handling
+				OnContextDestroying();
+				// Destroy context
+				DeleteContext();
+			}
+			// Destroy device context
+			_DeviceContext.DecRef();
+			_DeviceContext = null;
+
+			// Base implementation
+			base.OnUnrealized();
+		}
+
+		protected override bool OnDrawn(Context cr)
+		{
+			try {
+				MakeCurrentContext();
+
+				// Draw
+				OnRender();
+
+				// Update
+				OnContextUpdate();
+
+				// Swap
+				_DeviceContext.SwapBuffers();
+			} catch (Exception exception) {
+				
+			}
+
+			return (true);
+		}
+
+		#endregion
+	}
+
+	/// <summary>
+	/// Arguments for <see cref="GlWidget"/> events.
+	/// </summary>
+	public class GlWidgetEventArgs : System.EventArgs
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Construct a GlWidgetEventArgs.
+		/// </summary>
+		/// <param name="deviceContext">
+		/// The <see cref="DeviceContext"/> used for the underlying <see cref="GlWidget"/>.
+		/// </param>
+		/// <param name="renderContext">
+		/// The OpenGL context used for rendering.
+		/// </param>
+		public GlWidgetEventArgs(DeviceContext deviceContext, IntPtr renderContext)
+		{
+			if (deviceContext == null)
+				throw new ArgumentNullException("deviceContext");
+			if (renderContext == IntPtr.Zero)
+				throw new ArgumentException("renderContext");
+
+			DeviceContext = deviceContext;
+			RenderContext = renderContext;
+		}
+
+		#endregion
+
+		#region Event Arguments
+
+		/// <summary>
+		/// The <see cref="DeviceContext"/> used for the underlying <see cref="GlWidget"/>.
+		/// </summary>
+		public readonly DeviceContext DeviceContext;
+
+		/// <summary>
+		/// The OpenGL context used for rendering.
+		/// </summary>
+		public readonly IntPtr RenderContext;
 
 		#endregion
 	}
