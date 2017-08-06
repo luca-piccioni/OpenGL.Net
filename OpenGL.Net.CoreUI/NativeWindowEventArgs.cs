ee388c5cdebeb65e487c1a8101eb7e716b6e329c
Luca Piccioni <luca.piccioni@gmail.com>
2017-08-06 21:46:14 +0200

    Sample running on .NET Framework using CoreUI.

---
+++ OpenGL.Net.CoreUI/NativeWindowEventArgs.cs
@@ -0,0 +1,66 @@
+﻿
+// Copyright (c) 2017 Luca Piccioni
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+
+namespace OpenGL.CoreUI
+{
+	public class NativeWindowEventArgs : EventArgs
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Construct a NativeWindowEventArgs.
+		/// </summary>
+		/// <param name="deviceContext">
+		/// The <see cref="DeviceContext"/> used for the underlying <see cref="NativeWindow"/>.
+		/// </param>
+		/// <param name="renderContext">
+		/// The OpenGL context used for rendering.
+		/// </param>
+		public NativeWindowEventArgs(DeviceContext deviceContext, IntPtr renderContext)
+		{
+			if (deviceContext == null)
+				throw new ArgumentNullException("deviceContext");
+			if (renderContext == IntPtr.Zero)
+				throw new ArgumentException("renderContext");
+
+			DeviceContext = deviceContext;
+			RenderContext = renderContext;
+		}
+
+		#endregion
+
+		#region Event Arguments
+
+		/// <summary>
+		/// The <see cref="DeviceContext"/> used for the underlying <see cref="GlControl"/>.
+		/// </summary>
+		public readonly DeviceContext DeviceContext;
+
+		/// <summary>
+		/// The OpenGL context used for rendering.
+		/// </summary>
+		public readonly IntPtr RenderContext;
+
+		#endregion
+	}
+}
---
+++ Samples/HelloTriangle.CoreUI/Program.cs
@@ -0,0 +1,71 @@
+﻿
+// Copyright (C) 2016-2017 Luca Piccioni
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+// USA
+
+using OpenGL;
+using OpenGL.CoreUI;
+
+namespace HelloTriangle.CoreUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (NativeWindow nativeWindow = NativeWindow.Create()) {
+				nativeWindow.ContextCreated += NativeWindow_ContextCreated;
+				nativeWindow.Render += NativeWindow_Render;
+
+				nativeWindow.Create(0, 0, 640, 480);
+				nativeWindow.Show();
+				nativeWindow.Run();
+			}
+		}
+
+		private static void NativeWindow_ContextCreated(object sender, NativeWindowEventArgs e)
+		{
+			NativeWindow nativeWindow = (NativeWindow)sender;
+
+			// Here you can allocate resources or initialize state
+			Gl.MatrixMode(MatrixMode.Projection);
+			Gl.LoadIdentity();
+			Gl.Ortho(0.0, 1.0, 0.0, 1.0, 0.0, 1.0);
+
+			// Uses multisampling, if available
+			if (nativeWindow.MultisampleBits > 0)
+				Gl.Enable(EnableCap.Multisample);
+		}
+
+		private static void NativeWindow_Render(object sender, NativeWindowEventArgs e)
+		{
+			NativeWindow nativeWindow = (NativeWindow)sender;
+
+			Gl.Viewport(0, 0, (int)nativeWindow.Width, (int)nativeWindow.Height);
+			Gl.Clear(ClearBufferMask.ColorBufferBit);
+
+			// Animate triangle
+			Gl.MatrixMode(MatrixMode.Modelview);
+			Gl.LoadIdentity();
+
+			// Old school OpenGL
+			Gl.Begin(PrimitiveType.Triangles);
+			Gl.Color3(1.0f, 0.0f, 0.0f); Gl.Vertex2(0.0f, 0.0f);
+			Gl.Color3(0.0f, 1.0f, 0.0f); Gl.Vertex2(0.5f, 1.0f);
+			Gl.Color3(0.0f, 0.0f, 1.0f); Gl.Vertex2(1.0f, 0.0f);
+			Gl.End();
+		}
+	}
+}
