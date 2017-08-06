2f6ac7c60268ee9fc2931f216603b2d04396a8ae
Ricky Curtice <kf6kjg@users.noreply.github.com>
2017-08-06 15:52:40 -0700

    Start of windowless triangle demo

---
+++ Samples/WindowlessTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+using OpenGL;
+
+namespace WindowlessTriangle
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string envDebug = Environment.GetEnvironmentVariable("DEBUG");
+			if (envDebug == "GL")
+			{
+				KhronosApi.Log += delegate (object sender, KhronosLogEventArgs e)
+				{
+					Console.WriteLine(e.ToString());
+				};
+				KhronosApi.LogEnabled = true;
+			}
+
+
+		}
+	}
+}
