76cde367a5b07c064c912fa0c4e23515805a5597
Luca Piccioni <luca.piccioni@gmail.com>
2017-09-14 20:58:21 +0200

    Skeleton for unit testing Quaternion. #63.

---
+++ OpenGL.Net.Test/Quaternion.cs
@@ -0,0 +1,44 @@
+﻿
+// Copyright (C) 2017 Luca Piccioni
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using NUnit.Framework;
+
+namespace OpenGL.Test
+{
+	/// <summary>
+	/// Tests for <see cref="Quaternion"/>.
+	/// </summary>
+	[TestFixture]
+	[Category("Math")]
+	class QuaternionTest
+	{
+		[Test]
+		public void TestQuaternion()
+		{
+			Quaternion q = new Quaternion(Vertex3f.UnitZ, 0.0f);
+
+			Assert.AreEqual(0.0f, q.RotationAngle);
+			Assert.AreEqual(Vertex3f.UnitZ, q.RotationVector);
+
+			q.RotationAngle = 90.0f;
+		}
+	}
+}
