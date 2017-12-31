6553990d7e5f4e73fd5a32fdb08ab70dffa4c632
Luca Piccioni <luca.piccioni@gmail.com>
2017-12-31 15:02:44 +0100

    Fixed x86/x64 configurations. Included test run scripts.

---
+++ RunTests_VC14.bat
@@ -0,0 +1,25 @@
+
+@CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat"
+
+@SET SLN_PATH=OpenGL.Net_VC14.sln
+@SET SLN_BUILD_OPTS=/verbosity:minimal
+
+msbuild %SLN_PATH% %SLN_BUILD_OPTS% /property:Configuration=Release /p:Platform="Any CPU"
+msbuild %SLN_PATH% %SLN_BUILD_OPTS% /property:Configuration=Release /p:Platform="x86"
+msbuild %SLN_PATH% %SLN_BUILD_OPTS% /property:Configuration=Release /p:Platform="x64"
+msbuild %SLN_PATH% %SLN_BUILD_OPTS% /property:Configuration=Debug   /p:Platform="Any CPU"
+msbuild %SLN_PATH% %SLN_BUILD_OPTS% /property:Configuration=Debug   /p:Platform="x86"
+msbuild %SLN_PATH% %SLN_BUILD_OPTS% /property:Configuration=Debug   /p:Platform="x64"
+
+@SET TEST_RUN=packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe
+@SET TEST_OPTS=--noheader
+@SET TEST_WHERE=--where="cat=GL || cat=EGL || cat =WGL || cat=GL_VERSION_1_0 || cat=Math || cat=Framework || cat=Toolkit_CoreUI"
+
+%TEST_RUN% %TEST_WHERE% %TEST_OPTS%       --out=OpenGL.Net_net461.xml            OpenGL.Net.Test\bin\net461\Release\OpenGL.Net.Test.dll
+%TEST_RUN% %TEST_WHERE% %TEST_OPTS%       --out=OpenGL.Net_net35.xml             OpenGL.Net.Test\bin\net35\Release\OpenGL.Net.Test.dll
+%TEST_RUN% %TEST_WHERE% %TEST_OPTS%       --out=OpenGL.Net.CoreUI_net461.xml     OpenGL.Net.CoreUI.Test\bin\net461\Release\OpenGL.Net.CoreUI.Test.dll
+%TEST_RUN% %TEST_WHERE% %TEST_OPTS% --x86 --out=OpenGL.Net_net461_x86.xml        OpenGL.Net.Test\bin\net461\Release\OpenGL.Net.Test.dll
+%TEST_RUN% %TEST_WHERE% %TEST_OPTS% --x86 --out=OpenGL.Net_net35_x86.xml         OpenGL.Net.Test\bin\net35\Release\OpenGL.Net.Test.dll
+%TEST_RUN% %TEST_WHERE% %TEST_OPTS% --x86 --out=OpenGL.Net.CoreUI_net461_x86.xml OpenGL.Net.CoreUI.Test\bin\net461\Release\OpenGL.Net.CoreUI.Test.dll
+
+pause
