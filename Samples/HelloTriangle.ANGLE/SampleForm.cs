d819cd0c77e177991e61d170d9e73202706cbd9e
Luca Piccioni <luca.piccioni@gmail.com>
2018-02-02 23:59:42 +0100

    Using Math structures.

--- Samples/HelloTriangle.ANGLE/SampleForm.cs
+++ Samples/HelloTriangle.ANGLE/SampleForm.cs
@@ -96,7 +96,7 @@ namespace HelloTriangle.ANGLE
 		private void RenderControl_Render(object sender, GlControlEventArgs e)
 		{
 			Control control = (Control)sender;
-			OrthoProjectionMatrix projectionMatrix = new OrthoProjectionMatrix(0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f);
+			Matrix4x4f mvp = Matrix4x4f.Ortho2D(0.0f, 1.0f, 0.0f, 1.0f);
 
 			Gl.Viewport(0, 0, control.Width, control.Height);
 			Gl.Clear(ClearBufferMask.ColorBufferBit);
@@ -112,7 +112,7 @@ namespace HelloTriangle.ANGLE
 				Gl.VertexAttribPointer((uint)_Es2_Program_Location_aColor, 3, VertexAttribType.Float, false, 0, arrayColor.Address);
 				Gl.EnableVertexAttribArray((uint)_Es2_Program_Location_aColor);
 
-				Gl.UniformMatrix4(_Es2_Program_Location_uMVP, false, projectionMatrix.ToArray());
+				Gl.UniformMatrix4f(_Es2_Program_Location_uMVP, 1, false, ref mvp);
 
 				Gl.DrawArrays(PrimitiveType.Triangles,  0, 3);
 			}
@@ -124,6 +124,8 @@ namespace HelloTriangle.ANGLE
 				Gl.DeleteProgram(_Es2_Program);
 			_Es2_Program = 0;
 		}
+
+		private float _Angle;
 
 		private uint _Es2_Program;
 
